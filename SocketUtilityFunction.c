455cc5036d99396365662a8e254eb7a676cbae4e
Vinoth Energetic <vinothcse2000@gmail.com>
2016-11-13 19:17:31 +0530

    Update SocketUtilityFunction.c

--- SocketUtilityFunction.c
+++ SocketUtilityFunction.c
@@ -1,8 +1,11 @@
 void getUserandHostName(char *p_output,int p_len)
 {
 	char l_buffer[p_len];	
-	gethostname(p_output,p_len);		
-	getlogin_r(l_buffer,p_len-strlen(p_output));
+		
+	getlogin_r(p_output,p_len);
+	gethostname(l_buffer,p_len-strlen(p_output));
+	
+	
 	strcat(p_output,"@");
 	strcat(p_output,l_buffer);
 	
